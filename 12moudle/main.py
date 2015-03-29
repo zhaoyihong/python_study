bff4142fae1ffc4561ed58537fa4ca5a43377d88
zhaoyihong <zhaoyihong@126.com>
2015-03-29 20:09:51 +0800

    模块笔记

---
+++ 12moudle/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import test 
+
+
+if __name__ == "__main__":
+
+    print test.add(100,100)
---
+++ 12moudle/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+def add(i,j):
+    '''
+        返回i+j的值
+    '''
+    return i+j
+
